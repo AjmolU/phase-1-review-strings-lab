a5052dafd448aa3d8b997c8f6b7e737f6e6765b6
Ajmol Uddin <uddin.ajmol@gmail.com>
2021-07-06 23:49:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = "AU";
+
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
